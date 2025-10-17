0f09a2d80cb2cdd090cf68e764c85384ea12c24e
Kaivalya Bachkar <kaivalya.bachkar@gmail.com>
2025-10-17 10:28:40 +0530

    All Task Done

--- 23find_replace.sh
+++ 23find_replace.sh
@@ -2,9 +2,6 @@
 
 #!/bin/bash
 
-# Usage: ./search_replace.sh /path/to/directory "search_text" "replace_text"
-
-# Check for correct number of arguments
 if [ "$#" -ne 3 ]; then
     echo "Usage: $0 /path/to/directory \"search_text\" \"replace_text\""
     exit 1
@@ -14,15 +11,11 @@ DIRECTORY=$1
 SEARCH=$2
 REPLACE=$3
 
-# Loop through all files in the directory
 for FILE in "$DIRECTORY"/*; do
     if [ -f "$FILE" ]; then
-        # Perform search and replace using sed
         sed -i "s/${SEARCH}/${REPLACE}/g" "$FILE"
         echo "Updated $FILE"
     fi
 done
 
 echo "Search and replace completed."
-
-#grep -rl "$search_text" "$target_dir" | xargs sed -i "s/$search_text/$replace_text/g"
